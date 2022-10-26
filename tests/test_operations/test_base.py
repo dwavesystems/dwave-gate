5a81653c536c0e230ad84532bd7acee113200c1a
Theodor Isacsson <theodor@isacsson.ca>
2022-10-26 14:32:52 -0700

    Add circuit and operation tests

---
+++ tests/test_operations/test_base.py
@@ -0,0 +1,211 @@
+import math
+
+import numpy as np
+import pytest
+
+import dwgms.operations.operations as ops
+from dwgms.circuit import Circuit
+from dwgms.operations.base import ABCLockedAttr, Operation, ParametricOperation, create_operation
+
+
+class TestLockedMetaclass:
+    """Unit tests for the ``ABCLockedAttr`` metaclass.
+
+    The list of locked attributes is part of the ``ABCLockedAttr`` metaclass. If adding a locked
+    attribute there, please update this test to also test that locked attribute.
+    """
+
+    def test_locked_attribute(self):
+        """Test that a locked class attribute cannot be changed."""
+
+        class DummyLocked(metaclass=ABCLockedAttr):
+            """Dummy class used to test the ``ABCLockedAttr`` metaclass."""
+
+            matrix = [[1, 0], [0, 1]]
+            not_matrix = 42
+
+        assert DummyLocked.not_matrix == 42
+        DummyLocked.not_matrix = 24
+        assert DummyLocked.not_matrix == 24
+
+        with pytest.raises(ValueError, match="Cannot set class attribute"):
+            DummyLocked.matrix = 5
+
+    def test_locked_attribute_subclass(self):
+        """Test that a locked class attribute cannot be changed in a subclass."""
+
+        class DummyLocked(metaclass=ABCLockedAttr):
+            """Dummy class used to test the ``ABCLockedAttr`` metaclass."""
+
+            matrix = [[1, 0], [0, 1]]
+            not_matrix = 42
+
+        class DummySub(DummyLocked):
+            pass
+
+        assert DummySub.not_matrix == 42
+        DummySub.not_matrix = 24
+        assert DummySub.not_matrix == 24
+
+        with pytest.raises(ValueError, match="Cannot set class attribute"):
+            DummySub.matrix = 5
+
+
+class TestMatrixRepr:
+    """Unit tests for all matrix representations of operations."""
+
+    @pytest.mark.parametrize(
+        "op, matrix",
+        [
+            (ops.X, np.array([[0, 1], [1, 0]])),
+            (ops.Y, np.array([[0.0, -1.0j], [1.0j, 0.0]])),
+            (ops.Z, np.array([[1, 0], [0, -1]])),
+            (ops.Hadamard, math.sqrt(2) / 2 * np.array([[1.0, 1.0], [1.0, -1.0]])),
+            (ops.RX(np.pi / 2), math.sqrt(2) / 2 * np.array([[1, -1j], [-1j, 1]])),
+            (ops.RY(np.pi / 2), math.sqrt(2) / 2 * np.array([[1, -1], [1, 1]])),
+            (ops.RZ(np.pi / 2), math.sqrt(2) / 2 * np.array([[1 - 1j, 0], [0, 1 + 1j]])),
+            (ops.Rotation([np.pi / 2] * 3), math.sqrt(2) / 2 * np.array([[-1j, -1], [1, 1j]])),
+            (ops.CX, np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])),
+            (ops.CX(0, 1), np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])),
+            (ops.CNOT, np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])),
+            (ops.CX(1, 0), np.array([[1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0]])),
+            (ops.CZ, np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, -1]])),
+            (
+                ops.SWAP,
+                np.array(
+                    [
+                        [1.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 1.0, 0.0],
+                        [0.0, 1.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 1.0],
+                    ]
+                ),
+            ),
+            (
+                ops.CSWAP,
+                np.array(
+                    [
+                        [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                    ]
+                ),
+            ),
+        ],
+    )
+    def test_matrix_repr(self, op, matrix):
+        """Test that matrix representations are correct."""
+        assert np.allclose(op.matrix, matrix)
+
+        # if matrix is accessed as a classproperty, then test it on an instance as well
+        if not isinstance(op, Operation):
+            assert np.allclose(op().matrix, matrix)
+
+
+@pytest.mark.xfail(reason="Measurements are not implemented yet.")
+class TestMeasurement:
+    """Unit tests for the ``Measurement`` class."""
+
+
+@pytest.mark.xfail(reason="Barriers are not implemented yet.")
+class TestBarrier:
+    """Unit tests for the ``Barrier`` class."""
+
+
+class TestCreateOperation:
+    """Unit tests for the ``create_operation`` function."""
+
+    def test_parametric(self):
+        """Test creating an operation out of a parametric circuit."""
+        circuit = Circuit(1, parametric=True)
+
+        with circuit.context as (p, q):
+            ops.RZ(p[0], q[0])
+            ops.RY(p[1], q[0])
+            ops.RZ(p[2], q[0])
+
+        RotOp = create_operation(circuit, label="Rot")
+        params = [0.23, 0.34, 0.45]
+        rot_op = RotOp(params)
+
+        assert RotOp.matrix is None
+        assert RotOp.label == "Rot"
+
+        assert np.allclose(rot_op.matrix, ops.Rotation(params).matrix)
+        assert rot_op.label == f"Rot({params})"
+
+    def test_non_parametric(self):
+        """Test creating an operation out of a non-parametric circuit."""
+        circuit = Circuit(1)
+
+        with circuit.context as q:
+            ops.Hadamard(q[0])
+            ops.X(q[0])
+            ops.Hadamard(q[0])
+
+        ZOp = create_operation(circuit, label="Z")
+
+        assert np.allclose(ZOp.matrix, ops.Z.matrix)
+        assert ZOp.label == "Z"
+
+    def test_no_label(self):
+        """Test creating an operation without a label."""
+        circuit = Circuit(1)
+
+        with circuit.context as q:
+            ops.Hadamard(q[0])
+            ops.X(q[0])
+            ops.Hadamard(q[0])
+
+        ZOp = create_operation(circuit)
+
+        assert ZOp.label == "CustomOperation"
+
+    def test_superclass(self):
+        """Test creating a (forced) ``Operation``."""
+        circuit = Circuit(1)
+
+        with circuit.context as q:
+            ops.Hadamard(q[0])
+            ops.X(q[0])
+            ops.Hadamard(q[0])
+
+        ZOp = create_operation(circuit, superclass=Operation)
+        assert issubclass(ZOp, Operation)
+
+    def test_superclass_parametric(self):
+        """Test creating a (forced) ``ParametricOperation``."""
+        circuit = Circuit(1, parametric=True)
+
+        with circuit.context as (p, q):
+            ops.RZ(p[0], q[0])
+            ops.RY(p[1], q[0])
+            ops.RZ(p[2], q[0])
+
+        ZOp = create_operation(circuit, superclass=ParametricOperation)
+        assert issubclass(ZOp, ParametricOperation)
+
+    def test_parametric_mix(self):
+        """Test creating an operation out of a parametric circuit with some hard-coded
+        parameters."""
+        circuit = Circuit(1, parametric=True)
+
+        with circuit.context as (p, q):
+            ops.RZ(0.1, q[0])
+            ops.RY(p[0], q[0])
+            ops.RZ(0.3, q[0])
+
+        RotOp = create_operation(circuit, label="Rot")
+        params = [0.2]
+        rot_op = RotOp(params)
+
+        assert RotOp.matrix is None
+        assert RotOp.label == "Rot"
+
+        assert np.allclose(rot_op.matrix, ops.Rotation([0.1, 0.2, 0.3]).matrix)
+        assert rot_op.label == "Rot([0.2])"
--- tests/test_registers.py
+++ tests/test_registers.py
@@ -1,7 +1,13 @@
 # Confidential & Proprietary Information: D-Wave Systems Inc.
 import pytest
 
-from dwgms.registers import ClassicalRegister, QuantumRegister
+from dwgms.registers import (
+    ClassicalRegister,
+    QuantumRegister,
+    RegisterError,
+    SelfIncrementingRegister,
+    Variable,
+)
 
 
 class TestQuantumRegister:
@@ -30,6 +36,13 @@ class TestQuantumRegister:
 
         for i, d in enumerate(reg):
             assert d == data[i]
+
+    def test_index(self):
+        """Test getting the index of an item in a register."""
+        data = [1, 13, 42, 54]
+        reg = QuantumRegister(label="reg", data=data)
+
+        assert reg.index(13) == data.index(13)
 
     def test_register_indexing(self):
         """Test getting an item at a specific index in a register."""
@@ -101,11 +114,17 @@ class TestQuantumRegister:
         with pytest.raises(ValueError, match="already in use"):
             reg.add(qubits)
 
-    @pytest.mark.xfail(reason="Freezing quantum registers not implemented yet.")
     def test_freeze(self):
         """Test freezing the register."""
         reg = QuantumRegister(label="reg", data=[0, 1])
         reg.freeze()
+
+        assert reg.frozen
+
+        with pytest.raises(
+            RegisterError, match="Register is frozen and no more data can be addded."
+        ):
+            reg.add("abc")
 
 
 class TestClassicalRegister:
@@ -134,6 +153,13 @@ class TestClassicalRegister:
 
         for i, d in enumerate(reg):
             assert d == data[i]
+
+    def test_index(self):
+        """Test getting the index of an item in a register."""
+        data = [1, 13, 42, 54]
+        reg = ClassicalRegister(label="reg", data=data)
+
+        assert reg.index(13) == data.index(13)
 
     def test_register_indexing(self):
         """Test getting an item at a specific index in a register."""
@@ -181,8 +207,161 @@ class TestClassicalRegister:
         with pytest.raises(ValueError, match="already in use"):
             reg.add(bits)
 
-    @pytest.mark.xfail(reason="Freezing classical registers not implemented yet.")
     def test_freeze(self):
         """Test freezing the register."""
         reg = ClassicalRegister(label="reg", data=[0, 1])
         reg.freeze()
+
+        assert reg.frozen
+
+        with pytest.raises(
+            RegisterError, match="Register is frozen and no more data can be addded."
+        ):
+            reg.add("abc")
+
+
+class TestSelfIncrementingRegister:
+    """Unit tests for the ``SelfIncrementingRegister`` class."""
+
+    def test_empty_register(self):
+        """Test initializing an empty register."""
+        reg = SelfIncrementingRegister(label="reg")
+
+        assert reg.label == "reg"
+        assert reg.data == []
+        assert len(reg) == 0
+
+    def test_register_with_data(self):
+        """Test initializing an empty data register."""
+        reg = SelfIncrementingRegister(label="reg", data=[1, 2, 3, 42])
+
+        assert reg.label == "reg"
+        assert reg.data == [1, 2, 3, 42]
+        assert len(reg) == 4
+
+    def test_register_iteration(self):
+        """Test iterating over a register."""
+        data = [1, 21, 32]
+        reg = SelfIncrementingRegister(label="reg", data=data)
+
+        for i, d in enumerate(reg):
+            assert d == data[i]
+
+    def test_index(self):
+        """Test getting the index of an item in a register."""
+        data = [1, 13, 42, 54]
+        reg = SelfIncrementingRegister(label="reg", data=data)
+
+        assert reg.index(13) == data.index(13)
+
+    def test_register_indexing(self):
+        """Test getting an item at a specific index in a register."""
+        data = [1, 13, 42, 54]
+        reg = SelfIncrementingRegister(label="reg", data=data)
+
+        # don't use enumerate so as to _only_ test indexing (and not iteration)
+        for i in range(len(reg)):
+            assert reg[i] == data[i]
+
+    def test_indexing_outside_scope(self):
+        """Test getting an item at an index outside the scope of the register."""
+        data = [1, 13]
+        reg = SelfIncrementingRegister(label="reg", data=data)
+
+        assert reg[4] == Variable("4")
+        assert reg.data == data + [Variable("2"), Variable("3"), Variable("4")]
+
+    def test_repr(self):
+        """Test the representation of a register."""
+        reg = SelfIncrementingRegister(label="reg", data=[1, 13, 42, 54])
+
+        assert reg.__repr__() == "<SelfIncrementingRegister, data=[1, 13, 42, 54]>"
+
+    def test_str(self):
+        """Test the string of a register."""
+        reg = SelfIncrementingRegister(label="reg", data=[1, 13, 42, 54])
+
+        assert str(reg) == "[1, 13, 42, 54]"
+
+    @pytest.mark.parametrize(
+        "bits, expected",
+        [
+            ("c0", ["c0"]),
+            (("cb0, cb1, cb2"), ["cb0, cb1, cb2"]),
+            (["banana", "mango"], ["banana", "mango"]),
+        ],
+    )
+    def test_add(self, bits, expected):
+        """Test adding bits to the register."""
+        reg = SelfIncrementingRegister(label="reg", data=[0, 1])
+        assert reg.data == [0, 1]
+
+        reg.add(bits)
+        assert reg.data == [0, 1] + expected
+
+    @pytest.mark.parametrize("bits", ["mango", ["banana", "mango"], ["coconut", "coconut"]])
+    def test_add_duplicate_data(self, bits):
+        """Test adding bits to the register using existing labels."""
+        reg = SelfIncrementingRegister(label="reg", data=["banana", "mango"])
+        assert reg.data == ["banana", "mango"]
+
+        with pytest.raises(ValueError, match="already in use"):
+            reg.add(bits)
+
+    def test_freeze(self):
+        """Test freezing the register."""
+        reg = SelfIncrementingRegister(label="reg", data=[0, 1])
+        reg.freeze()
+
+        assert reg.frozen
+
+        with pytest.raises(
+            RegisterError, match="Register is frozen and no more data can be addded."
+        ):
+            reg.add("abc")
+
+    def test_selfincrementing_when_frozen(self):
+        """Test freezing the register and then attempting to access outside of scope."""
+        reg = SelfIncrementingRegister(label="reg", data=[0, 1])
+        reg.freeze()
+
+        assert reg.frozen
+
+        with pytest.raises(IndexError):
+            reg[15]
+
+
+class TestVariable:
+    """Unit tests for the ``Variable`` class."""
+
+    def test_initialize(self):
+        """Test initializing a variable."""
+        name = "ananas"
+        var = Variable(name)
+
+        assert var.name == name
+
+    def test_hash(self):
+        "Test that two equal variables have the same hash."
+        name = "ananas"
+        var_0 = Variable(name)
+        var_1 = Variable(name)
+
+        assert var_0 == var_1
+        assert hash(var_0) == hash(var_1)
+
+    def test_equality(self):
+        "Test equality of two variables with the same name."
+        name = "ananas"
+        var_0 = Variable(name)
+        var_1 = Variable(name)
+
+        assert var_0 == var_1
+        assert var_0 is not var_1
+
+        assert var_0 != name
+
+    def test_repr(self):
+        "Test the representation of a variable."
+        var = Variable("ananas")
+        assert var.__repr__() == "{ananas}"
