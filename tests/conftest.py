d201be46f8cbf19e5eab1721c3841107e691c56c
Theodor Isacsson <theodor@isacsson.ca>
2022-11-24 15:45:38 -0800

    Add license and headers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,19 @@
-# Confidential & Proprietary Information: D-Wave Systems Inc.
+# Copyright 2022 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 import pytest
+from typing import Type
 
 import dwave.gate.operations as ops
 from dwave.gate.circuit import Circuit, ParametricCircuit
@@ -90,7 +104,7 @@ def two_qubit_controlled_op(monkeypatch):
 
         _num_control: int = 1
         _num_target: int = 1
-        _target_operation = ops.X
+        _target_operation: Type[Operation] = ops.X
 
     monkeypatch.setattr(DummyOp, "__abstractmethods__", set())
 
