7cd24f7e4d1ecc0d5589f2848781f845f3f3e1a0
William Bernoudy <wbernoudy@dwavesys.com>
2022-11-25 18:30:25 -0800

    Add license header to simulator files

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,11 +1,24 @@
-# Confidential & Proprietary Information: D-Wave Systems Inc.
+# Copyright 2022 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 import pytest
 import numpy as np
 
 import dwave.gate.operations as ops
 from dwave.gate.operations.operations import __all__ as all_ops
-from dwave.gate.circuit import Circuit, ParametricCircuit
-from dwave.gate.simulator.simulator import simulate, apply_instruction
+from dwave.gate.circuit import Circuit
+from dwave.gate.simulator.simulator import simulate
 
 
 def test_simulate_sv_empty_circuit(empty_circuit):
