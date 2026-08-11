7d42d26f99c350884a9a957957ea1da5a91eaf69
thisac <theodor@isacsson.ca>
2026-08-10 18:26:44 -0700

    Remove aqusim references

--- tests/test_qcdl_circuit.py
+++ tests/test_qcdl_circuit.py
@@ -35,7 +35,7 @@ from dwave.gate.qcdl.qcdl_models import Qcdl
 
 
 def _check_serializable(data):
-    # Aqusim uses multiprocessing which requires that qcdls are pickleable.
+    # ensure that qcdls are pickleable
     assert pickle.loads(pickle.dumps(data)) == data
     raw = data.model_dump(exclude_unset=True)
     assert json.loads(json.dumps(raw)) == raw
