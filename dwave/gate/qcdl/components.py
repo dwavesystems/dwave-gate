15e1b975045a5e48ae725f1db0e5a2bbca8a8081
Amos Anderson <anderson@quantumcircuits.com>
2026-08-25 11:26:32 -0400

    improve wording in example

--- dwave/gate/qcdl/components.py
+++ dwave/gate/qcdl/components.py
@@ -1334,8 +1334,10 @@ class QCDLModuleContainer(QCDLModuleContainerBase):
                     sc = Scope(q0, q1)
                     r1 = sc.Register(name="r1")
                     h(q0)
-                    # alias=True reuses the memory r1 already allocated, so the
-                    # outcome is stored on q0 only rather than mirrored
+                    # alias=True reuses the memory r1 already allocated, so this
+                    # illustrates a way to store this outcome only on q0 rather
+                    # than mirrored to all the qubits in the Scope where r1 was
+                    # originally allocated.
                     measure(q0, register=q0.Register(name="r1", alias=True))
                     sc.all_to_all(send=r1==1, reduce_op="&")
                     with sc.If(None):
