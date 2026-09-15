12a448da2d87c394af1785fd9c939488fc4edb59
Trevor Keen <110417262+qci-keen@users.noreply.github.com>
2026-09-15 12:28:36 -0700

    Update examples/classical-arithmetic.py
    Co-authored-by: Joel Pasvolsky <34041130+JoelPasvolsky@users.noreply.github.com>

--- examples/classical-arithmetic.py
+++ examples/classical-arithmetic.py
@@ -19,7 +19,7 @@ from dwave.gate.utils.display import print_qcdl
 logging.basicConfig(level=logging.INFO)
 
 # %% [markdown]
-# ## 5-qubit Example
+# ## Five-Qubit Example
 # We will prepare a circuit with $n$ `controlling_qubits` and one `main_qubit`. We first apply a set of gates on the `controlling_qubits` and measure them. Denote the measurement outcome as $x_0, x_1, .., x_{n-1}$, we then apply a $R_y(\theta)$ gate on the `main_qubit` with angle
 # $$
 # \theta = \pi \sum_{j=0}^{n-1} x_j2^{-j-1}
